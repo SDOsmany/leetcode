f4e4f995d299844e6c3783b6f63063cecd9fb91e
Osmany <58539319+SDOsmany@users.noreply.github.com>
2024-11-05 23:01:50 -0800

    Time: 30 ms (19.54%) | Memory: 16.9 MB (87.14%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -4,6 +4,5 @@ class Solution:
         for i in range(0, len(s), 2):
             _slice = s[i:i+2]
             if _slice != "11" and _slice != "00":
-                print("not here", _slice)
                 count += 1
         return count
