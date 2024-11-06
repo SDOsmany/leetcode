d079585c5312e3a70dde468e60d019668631fbc0
Osmany <58539319+SDOsmany@users.noreply.github.com>
2024-11-05 23:04:33 -0800

    Time: 23 ms (43.68%) | Memory: 16.9 MB (87.14%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,8 +1,8 @@
 class Solution:
     def minChanges(self, s: str) -> int:
         count = 0
-        for i in range(0, len(s) -1, 2):
-            _slice = s[i:i+2]
-            if _slice != "11" and _slice != "00":
+        for i in range(0, len(s)-1, 2):
+            x,y = s[i], s[i+1]
+            if x != y:
                 count += 1
         return count
